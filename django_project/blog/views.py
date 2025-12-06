f83e930d68c56d467e1e589077cfa1a0e18624a5
Mahmoud <mahmoud.saadallah73@gmail.com>
2025-12-06 11:38:31 +0200

    Refactor blog views to use Post model for dynamic content and register Post model in admin

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Mahmoud',
-        'title': 'Blog Post 1',
-        'content' : 'first post content',
-        'date_posted' : 'August 27, 2018'
-    },
-    {
-        'author': 'Omar',
-        'title': 'Blog Post 2',
-        'content' : 'second post content',
-        'date_posted' : 'August 28, 2018'
-    }
-]
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context=context)
 
