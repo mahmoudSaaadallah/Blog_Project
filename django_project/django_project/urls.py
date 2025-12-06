40dda13d9b82b39763863f68c704a8d62ddecc88
Mahmoud <mahmoud.saadallah73@gmail.com>
2025-12-06 17:56:30 +0200

    Add user authentication views and templates for login and logout functionality fix the navbar to display login if the user is not authenticated and logout if the user is authenticated. fixing the problem of the post for logout

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,4 +1,4 @@
-
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
@@ -7,4 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("blog.urls")),
     path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', user_views.logout_view, name='logout'),
 ]
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth import logout
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -9,10 +10,14 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Account created for {username}!")
-            return redirect('blog-home')
+            messages.success(request, f"Your account has been created! You are now able to login.")
+            return redirect('login')
     else:
             form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'users/logout.html')
 
 
