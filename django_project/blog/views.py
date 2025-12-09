f78b5bcae860064b481f9bf08c01b425be2e273e
Mahmoud <mahmoud.saadallah73@gmail.com>
2025-12-09 10:40:22 +0200

    Refactor blog views to use class-based views for post listing and post detail

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
 
@@ -8,6 +9,21 @@ def home(request):
         'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context=context)
+
+class PostListView(ListView):
+    model = Post
+    # Template name by default appname/modelname_viewtype.html => blog/post_list.html
+    template_name = 'blog/home.html'
+
+    # Contect_object_name by default post_list then we have to override it.
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    # for the template we wouldn't chang it as we are going to use the default name.
+    context_object_name = 'post'
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'},)
