6a97d8a59d2c0ec4a4479097aead931a0a6f4d44
Mahmoud <mahmoud.saadallah73@gmail.com>
2025-12-10 22:10:25 +0200

    Add UserPostListView to display posts by a specific user with pagination and handel its url

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import(
     ListView,
@@ -41,6 +42,18 @@ class PostListView(ListView):
     def get_queryset(self):
         return Post.objects.select_related('author').all() 
     
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        posts = Post.objects.filter(author=user).order_by('-date_posted')
+        return posts
 
 class PostDetailView(DetailView):
     model = Post
